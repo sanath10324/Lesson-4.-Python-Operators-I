dbc7f3e85d8c902e8eb6a68e83b387bf9582bd2a
sanath10324 <sanathkatkamgamer@gmail.com>
2025-12-31 14:43:12 +0400

    Average of 5 trees

---
+++ average.py
@@ -0,0 +1,11 @@
+tree1 = 25
+tree2 = 56
+tree3 = 39
+tree4 = 87
+tree5 = 67 
+
+sum = tree1+tree2+tree3+tree4+tree5
+print("The sum of all the 5 trees are:", sum)
+
+average = sum/5
+print("The average of all the 5 trees is", average)
